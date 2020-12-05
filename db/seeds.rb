3311676b46d0e84ec3a5a7f0894092423a264e40
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-12-05 18:48:58 -0500

    Add seed creating airlines and reviews instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+airlines = Airline.create([
+  {
+    name: "United Airlines",
+    image_url: 'https://open-flights.s3.amazonaws.com/United-Airlines.png'
+  },
+  {
+    name: 'Southwest',
+    image_url: 'https://open-flights.s3.amazonaws.com/Aouthwest-Airlines.png'
+  },
+  {
+    name: 'Alaska Airlines',
+    image_url: 'https://open-flights.s3.amazonaws.com/Alaska-Airlines.png'
+  },
+  {
+    name: 'JetBlue',
+    image_url: 'https://open-flights.s3.amazonaws.com/JetBlue.png'
+  },
+  {
+    name: 'American Airlines',
+    image_url: 'https://open-flights.s3.amazonaws.com/American-Airlines.png'
+  }
+])
+
+reviews = Review.create([
+  {
+    title: 'Great airline',
+    description: 'This is the first review',
+    score: 5,
+    airline: airlines.first
+  },
+  {
+    title: 'Bad airline',
+    description: 'This is the first bad description',
+    score: 1,
+    airline: airlines.first
+  }
+])
