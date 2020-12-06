98b808d5176ec678211fdbc227f9a45bdf423491
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-12-05 19:51:56 -0500

    Add reviews controller

---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,32 @@
+module Api
+  module V1
+    class ReviewsController < ApplicationController
+      protect_from_forgery with: :null_session
+      def create
+        review = Review.new(review_params)
+
+        if review.save
+          render json: ReviewSerializer.new(review).serialized_json
+        else
+          render json: { error: review.errors.messages }, status: 442
+        end
+      end
+
+      def destroy
+        review = Review.find(params[:id])
+
+        if review.delete
+          head :no_content
+        else
+          render json: { error: review.errors.messages }, status: 442
+        end
+      end
+
+      private
+
+      def review_params
+        params.require(:review).permit(:title, :description, :score, :airline_id)
+      end
+    end
+  end
+end
